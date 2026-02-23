6be9bc8669eb760d48092bf64daf5bda93122a9a
Yiteng Peng <75665738+Yiteng-Peng@users.noreply.github.com>
2026-02-23 15:02:24 +0800

    Update latest

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-# VER=$(date +%Y%m%d%H%M)
+VER=$(date +%Y%m%d%H%M)
 VER=latest
-# sed -i '' "s/v=[0-9]\{8,12\}/v=$VER/g" index.html fiona/index.html
+sed -i '' "s/v=[0-9]\{8,12\}/v=$VER/g" index.html fiona/index.html
 git add .
 git commit -m "Update $VER"
 git push
